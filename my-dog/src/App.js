b3a2d386b033f0e446d2741088ab5f9aff01498f
SashaKa <katushkovasa@gmail.com>
2017-11-15 23:13:32 +0300

    array render

--- my-dog/src/App.js
+++ my-dog/src/App.js
@@ -1,41 +1,35 @@
-import React, { Component } from 'react';
-import logo from './dog-paw.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './dog-paw.svg'
+import './App.css'
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      dogs: []
+      breeds: []
     }
-    
   }
 
   componentDidMount() {
     fetch('https://dog.ceo/api/breeds/list')
-      .then(responce => responce.json())
-      .then(({results: dogs})=>this.setState ({dogs}) )
-      console.log(this.state.dogs)
-  }  
-
+      .then(response => response.json())
+      .then(({ message }) => this.setState({
+        breeds: message
+      }))
+  }
 
   render() {
-            
-      
-
-             return <div className="App">
-                 <header className="App-header">
-                   <img src={logo} className="App-logo" alt="logo" />
-                   <h1 className="App-title">
-                     Hello! Choose your Dog
-                   </h1>
-                 </header>
-                 <p className="App-intro">choose here</p>
-                 <div className="Dogs">
-                   <a href="{this.state}">TEST DOG</a>
-                 </div>
-               </div>
-           }
+    return <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Hello! Choose your Dog</h1>
+        </header>
+        <div className="Dogs">
+          {this.state.breeds.map((breed, i) => 
+            (<li className="dogEntry" key={i}>{breed}</li>))}
+        </div>
+      </div>
+  }
 }
 
-export default App;
+export default App
