0a8fffbf12556358fc94e9873518f1f898f52d89
SashaKa <katushkovasa@gmail.com>
2017-11-15 16:21:49 +0300

    empty array

--- my-dog/src/App.js
+++ my-dog/src/App.js
@@ -6,31 +6,36 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      status: []
+      dogs: []
     }
+    
   }
 
   componentDidMount() {
-    fetch('https://dog.ceo/api/breeds/list/all')
+    fetch('https://dog.ceo/api/breeds/list')
       .then(responce => responce.json())
-      .then(data=>this.setState ({status:data.status}))
+      .then(({results: dogs})=>this.setState ({dogs}) )
+      console.log(this.state.dogs)
   }  
 
 
   render() {
-    // const {status} = this.state;
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Hello! Choose your Dog</h1>
-        </header>
-        <p className="App-intro">choose here</p>
-        <div className="Dogs">
-        <a href='{this.state.message}'>TEST DOG</a></div>
-      </div>
-    )
-  }
+            
+      
+
+             return <div className="App">
+                 <header className="App-header">
+                   <img src={logo} className="App-logo" alt="logo" />
+                   <h1 className="App-title">
+                     Hello! Choose your Dog
+                   </h1>
+                 </header>
+                 <p className="App-intro">choose here</p>
+                 <div className="Dogs">
+                   <a href="{this.state}">TEST DOG</a>
+                 </div>
+               </div>
+           }
 }
 
 export default App;
